232f1e74f572ae9f97a4f76595202dfc2502fc5d
Mika88 <mikaspivak@gmail.com>
2019-01-05 23:06:38 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,4 +1,9 @@
 module ArtistsHelper
-  def display_artist
+  def display_artist(song)
+    if song.artist
+      link_to("Artist", song.artist)
+    else
+      link_to("Add Artist", song)
+    end
   end
 end
