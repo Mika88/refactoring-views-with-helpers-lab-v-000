28023c599d5e5d128a85b2d7ea780336bf114550
Mika88 <mikaspivak@gmail.com>
2019-01-05 23:09:05 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist
       link_to("Artist", song.artist)
     else
-      link_to("Add Artist", song)
+      link_to("Add Artist", edit_song_path(song))
     end
   end
 end
