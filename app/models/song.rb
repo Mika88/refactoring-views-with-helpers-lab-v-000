e90bac11f9148d9a107b8d847b542a854b441e2e
Mika88 <mikaspivak@gmail.com>
2019-01-05 22:34:31 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,11 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    binding.pry
     self.artist.name
   end
 
   def artist_name=(name)
-    name = self.artist.name
+    binding.pry
+    self.artist.name
   end
 end
